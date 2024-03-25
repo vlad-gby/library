e8ab13c895b82877f488317badfb578ed5acb224
Vladyslav Hubanov <155400906+vlad-gby@users.noreply.github.com>
2024-03-25 22:45:12 +0200

    Finish UI

--- script.js
+++ script.js
@@ -37,4 +37,24 @@ statusBtns.forEach(button => {
   });
 });
 
+// OPEN A GOOGLE BOOKS
+const hyperlinks = [];
+hyperlinks.push(document.querySelector('.card:nth-child(1) .hyperlink'), document.querySelector('.card:nth-child(2) .hyperlink'), document.querySelector('.card:nth-child(3) .hyperlink'));
+
+function attachHyperlink(element, link){
+  element.addEventListener('mouseup', e => {
+    window.open(link, '_blank');
+  });
+};
+
+attachHyperlink(hyperlinks[0], 'https://books.googleusercontent.com/books/content?req=AKW5Qaefla-E5X81NGARGOYiJiZrsiMJE1ZteeEYCWBsZC3t11etwnwpy2Bkog8Y-qHvOvBTd4kmWikXF5DldJLT_KZ4_Cxw5Jvi3nPNUuL-H_lNiQqigH5XXDYC1ourBGiM9N2kn_ZQ_2q9FVDXuRgSkSZ0n_jZkQCpzjNT_rbWzeM63dd0X-Huqz6Hjo93xUFJEhFFsYRsqQFFMpgneNJqTv1PpmCJFJN86TYbaKa4Tch8sSFrj8onCWyqauUlZpEa_XPfagxGikHpzxDwsiFBNa5JjGmYzw');
+
+attachHyperlink(hyperlinks[1], 'https://www.arvindguptatoys.com/arvindgupta/oldmansea.pdf');
+
+attachHyperlink(hyperlinks[2], 'https://www.crisrieder.org/thejourney/wp-content/uploads/2021/02/Jonathan-Livingston-Seagull.pdf');
+
+
+
+
+
 

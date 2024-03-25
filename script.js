2fed08a1a8ebe59c4460bc60d7fffbf1616ddf56
Vladyslav Hubanov <155400906+vlad-gby@users.noreply.github.com>
2024-03-25 20:03:10 +0200

    Made UI, started: .status and shadows/borders

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-let books = ['aook2', 'book1'];
+// ADD EVENT-LISTENERS TO BUTTONS
+const allButtons = document.querySelectorAll('button');
 
-console.log(books.sort())
+allButtons.forEach(button => {
+  button.addEventListener('mouseover', e => {
+    button.style.backgroundColor = changeColor(button, -20);
+  });
+  button.addEventListener('mouseout', e => {
+    button.style.backgroundColor = changeColor(button, 20);
+  });
+  button.addEventListener('mousedown', e => {
+    button.style.backgroundColor = changeColor(button, -20);
+  });
+  button.addEventListener('mouseup', e => {
+    button.style.backgroundColor = changeColor(button, 20);
+  });
+});
+
+function changeColor(element, amount){
+  const color = getComputedStyle(element).backgroundColor;
+  const rawColor = color.slice(4, -1).split(', ');
+  return `rgb(${rawColor.map(num => Number(num) + amount)})`;
+}
+
+const statusBtns = document.querySelectorAll('.status img:not(.selected)');
+
+statusBtns.forEach(button => {
+  button.addEventListener('mouseover', e => {
+    button.style.backgroundColor = 'rgb(130, 109, 83)';
+  });
+  button.addEventListener('mouseout', e => {
+    button.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+  });
+  button.addEventListener('mouseup', e => {
+
+    button.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+  });
+});
 
 
